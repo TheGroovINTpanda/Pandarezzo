1130567636a8a4d63b5b91d72c379e13be2158c4
Sam <samuel.rac@telecom-sudparis.eu>
2018-02-13 20:31:44 +0100

    création niveau1 : classe et xml. Modification du gitignore

--- app/src/main/java/com/example/samuel/pandarezzo/Bouton.java
+++ app/src/main/java/com/example/samuel/pandarezzo/Bouton.java
@@ -3,5 +3,15 @@ package com.example.samuel.pandarezzo;
 public class Bouton {
 
     String name;
-    
+    //visuel
+    //position
+    Son son;
+    Note note;
+
+    public void clic(){
+
+    }
+
+    @Override
+    public String toString() { return name;    }
 }
--- app/src/main/java/com/example/samuel/pandarezzo/Niveau.java
+++ app/src/main/java/com/example/samuel/pandarezzo/Niveau.java
@@ -1,4 +1,20 @@
 package com.example.samuel.pandarezzo;
 
 public abstract class Niveau {
+
+    String name;
+    //background;
+    Portee portee;
+    Bouton[] boutons;
+    int Score;
+    Panda panda;
+
+    public abstract void update();
+
+    public abstract void setScore();
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
