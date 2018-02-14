fae401ea122c46b65dc835eef25ca016fd04bec5
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-02-14 15:54:38 +0100

    ajout d'attribus et de méthodes update dans les principales classes

--- app/src/main/java/com/example/samuel/pandarezzo/GameEngine.java
+++ app/src/main/java/com/example/samuel/pandarezzo/GameEngine.java
@@ -1,4 +1,13 @@
 package com.example.samuel.pandarezzo;
 
 public class GameEngine {
+    private Panda panda;
+    private Note note;
+    private Niveau niveau;
+    private Bouton bouton;
+    private Son son;
+    private Portee portee;
+
+
+
 }
--- app/src/main/java/com/example/samuel/pandarezzo/Panda.java
+++ app/src/main/java/com/example/samuel/pandarezzo/Panda.java
@@ -1,4 +1,12 @@
 package com.example.samuel.pandarezzo;
 
+
 public class Panda {
+    //Position du Panda (xml?)
+
+    public void updateposition(Bouton bouton){
+
+    }
+
+
 }
