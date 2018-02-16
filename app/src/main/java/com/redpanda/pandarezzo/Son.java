b958e3c95356aa7d074c328d6634198e9f8bcc0e
Sam <samuel.rac@telecom-sudparis.eu>
2018-02-16 20:04:36 +0100

    petite maj de la classe son

--- app/src/main/java/com/redpanda/pandarezzo/Son.java
+++ app/src/main/java/com/redpanda/pandarezzo/Son.java
@@ -1,4 +1,21 @@
 package com.redpanda.pandarezzo;
 
+import android.content.Context;
+import android.content.Context;
+import android.media.MediaPlayer;
+
 public class Son {
+    private Note note;
+    private Context context;
+    Son(Note n, Context c){
+        note=n;
+        context=c;
+    }
+    void play(){
+//        MediaPlayer son = MediaPlayer.create(context,res.note.getName()); //** On créer une instance de MediaPlayer a partir du fichier audio correspondant à la note se trouvant dans les ressources
+//        Ce fichier n'existe pas encore donc forcemment il y a des erreurs
+//        son.start();
+    }
+    //**Cette methode permet de jouer le son de la note, elle sera activé dès que l'utilisateur apuyera sur le bouton
+
 }
