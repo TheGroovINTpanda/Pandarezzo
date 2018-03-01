384767618896e4348fca2046837427905223fe59
anne-claire <anne-claire.marechal@telecom-sudparis.eu>
2018-03-01 11:05:46 +0100

    ajout des fichiers son et completion de play()

--- app/src/main/java/com/redpanda/pandarezzo/Son.java
+++ app/src/main/java/com/redpanda/pandarezzo/Son.java
@@ -6,16 +6,22 @@ import android.media.MediaPlayer;
 
 public class Son {
     private Note note;
-    private Context context;
-    Son(Note n, Context c){
+    Son(Note n){
         note=n;
-        context=c;
     }
     void play(){
-//        MediaPlayer son = MediaPlayer.create(context,res.note.getName()); //** On créer une instance de MediaPlayer a partir du fichier audio correspondant à la note se trouvant dans les ressources
+        MediaPlayer son = MediaPlayer.create(getApplicationcontext(),res.notes.note.getName()); //** On créer une instance de MediaPlayer a partir du fichier audio correspondant à la note se trouvant dans les ressources
 //        Ce fichier n'existe pas encore donc forcemment il y a des erreurs
-//        son.start();
+        son.start();
+        son.setOnCompletionListener(new OnCompletionListener()
+        {
+            @Override
+            // Libérons les ressources lorsque la musique est terminée
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
     }
-    //**Cette methode permet de jouer le son de la note, elle sera activé dès que l'utilisateur apuyera sur le bouton
+    //**Cette methode permet de jouer le son de la note, elle sera activé dès que l'utilisateur apuyera sur le bouton, la methode est instencier pour libérer la ressource ès que le fichier joué est terminé.
 
 }
