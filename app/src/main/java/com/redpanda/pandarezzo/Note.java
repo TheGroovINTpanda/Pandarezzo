cebb61f3833aa63561f86d5a8bc90ef7c6a3089d
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-03-05 15:35:19 +0100

    constructeur de Note (ajout de Bitmap)

--- app/src/main/java/com/redpanda/pandarezzo/Note.java
+++ app/src/main/java/com/redpanda/pandarezzo/Note.java
@@ -1,7 +1,19 @@
 package com.redpanda.pandarezzo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class Note {
-    String name;
-    double x;
-    double y;
+    private String name;
+    private double x;
+    private double y;
+    private Bitmap notebitmap;
+
+    public Note(String name, int x, int y, Bitmap notebitmap){
+        this.name=name;
+        this.x=x;
+        this.y=y;
+        this.notebitmap=notebitmap;
+    }
+
 }
