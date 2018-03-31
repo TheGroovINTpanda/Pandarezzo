464411e83de5aeebb4e0e78b7cd747a0b7a679c8
Sam <samuel.rac@telecom-sudparis.eu>
2018-03-31 19:55:22 +0200

    tests et debbug

--- app/src/main/java/com/redpanda/pandarezzo/Bouton.java
+++ app/src/main/java/com/redpanda/pandarezzo/Bouton.java
@@ -40,16 +40,15 @@ public class Bouton extends android.support.v7.widget.AppCompatImageButton {
         this.imageButton = (ImageButton) activity.findViewById(buttonRef);
         this.son = new Son(context, sonRef);
         this.engine = engine;
-        imageButton.setOnTouchListener(new OnTouchListener() {
+        imageButton.setOnClickListener(new OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+            public void onClick(View view) {
                 son.play();
                 System.out.print(name);
                 TextView textView = (TextView) activity.findViewById(R.id.textViewDebug);
                 textView.setText(name);
                 touched = true;
                 engine.touched(name);
-                return false;
             }
         });
     }
