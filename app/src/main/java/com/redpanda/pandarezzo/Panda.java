3e43a82b25ec5992f657e76e4a020f39a87b7071
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-04-02 17:24:22 +0200

    Panda n'est plus une view mais une classe java

--- app/src/main/java/com/redpanda/pandarezzo/Panda.java
+++ app/src/main/java/com/redpanda/pandarezzo/Panda.java
@@ -7,34 +7,18 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.View;
 
-public class Panda extends View {
+public class Panda  {
     private Paint paint =new Paint(Paint.ANTI_ALIAS_FLAG);
     private int xpanda;
     private int ypanda;
 
-    //TODO: Un drawable du panda
-    private Bitmap notebitmap= BitmapFactory.decodeResource(getResources(),R.drawable.redpanda);
 
-    public Panda(Context context){
-        super(context);
 
+    public Panda(){
     }
 
-    @Override
-    public void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        if(animation()){
-            //TODO: un drawable du panda qui tire la langue (ou autre)
-            notebitmap=BitmapFactory.decodeResource(getResources(),R.drawable.redpanda);
-        }
-        canvas.drawBitmap(notebitmap,xpanda,ypanda,paint);
 
-    }
 
-    /*
-    TODO: réfléchir à un listener de Bouton pour savoir si la personne a bien répondu ou pas,
-    l'animation peut soit être un panda qui tire la langue ou un panda qui sautille
-     */
     public boolean animation(){
         return true;
     }
