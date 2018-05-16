0d6512f525091fd45dd082ea39ef9077cb98e3ef
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-05-16 16:48:23 +0200

    code pour faire dancer la note fait : ce qui reste : les drawables et savoir quand est ce que le niveau est terminé (GameEngine?)+ OnCreate du Niveau changé (ajout du layout de la note qui dance)

--- app/src/main/java/com/redpanda/pandarezzo/DancingNote.java
+++ app/src/main/java/com/redpanda/pandarezzo/DancingNote.java
@@ -5,11 +5,19 @@ import android.graphics.drawable.AnimationDrawable;
 import android.widget.ImageView;
 
 public class DancingNote {
-    private ImageView note;
+    private ImageView noteView;
     private AnimationDrawable dancingNote;
 
 
     public DancingNote(Activity activity){
+        this.noteView = activity.findViewById(R.id.dancingNote);
+        //TODO: changer l'image de la noteView ansi que le fichier dancing_note.xml
+        noteView.setBackgroundResource(R.drawable.dancing_note);
+        dancingNote= (AnimationDrawable) noteView.getBackground();
+    }
 
+
+    public void move(){
+        dancingNote.start();
     }
 }
--- app/src/main/java/com/redpanda/pandarezzo/Niveau.java
+++ app/src/main/java/com/redpanda/pandarezzo/Niveau.java
@@ -1,7 +1,11 @@
 package com.redpanda.pandarezzo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class Niveau extends AppCompatActivity{
 
@@ -10,12 +14,12 @@ public class Niveau extends AppCompatActivity{
     private Bouton[] boutons;
     private int Score;
     private Panda panda;
-
+    private boolean levelIsFinished;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.niveau);
-
+        levelIsFinished=false;
 
         /** Renseigner la portée de ce niveau dans level. */
 
@@ -31,10 +35,27 @@ public class Niveau extends AppCompatActivity{
         Bouton sol = new Bouton("Sol", this, engine, R.id.sol_bulle, R.raw.sol);
         Bouton la = new Bouton("La", this, engine, R.id.la_bulle, R.raw.la);
         Bouton si = new Bouton("Si", this, engine, R.id.si_bulle, R.raw.si);
-    }
+        //TODO: savoir quand est-ce que le level est terminé, passer par le gameEngine?
+        if(levelIsFinished){
+            setContentView(R.layout.final_note_dancing);
+            DancingNote dancingNote=new DancingNote(this);
+            dancingNote.move();
+            Button button = (Button) findViewById(R.id.nextLevel);
+            button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    Intent intent = new Intent(Niveau.this,Niveau.class);
+                    startActivity(intent);
+                    return false;
+                }
 
+            });
+
+        }
+    }
     @Override
     public String toString() {
         return name;
     }
 }
+
