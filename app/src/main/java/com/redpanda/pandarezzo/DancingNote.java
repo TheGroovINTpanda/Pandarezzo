4c8e383b83d2d8b1adeff25288ffdaeaeac21467
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-05-16 15:41:45 +0200

    creation d'une classe DancingNote

---
+++ app/src/main/java/com/redpanda/pandarezzo/DancingNote.java
@@ -0,0 +1,15 @@
+package com.redpanda.pandarezzo;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.ImageView;
+
+public class DancingNote {
+    private ImageView note;
+    private AnimationDrawable dancingNote;
+
+
+    public DancingNote(Activity activity){
+
+    }
+}
