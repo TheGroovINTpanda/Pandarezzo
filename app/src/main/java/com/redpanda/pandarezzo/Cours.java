644eb6008a7044d44c1c14602562e389915e2406
Sam <samuel.rac@telecom-sudparis.eu>
2018-05-17 17:48:47 +0200

    Fix problème de compilation : import de lib, changement Button en ImageButton, on a retirer la classe cours2 car on peut changer les layout sans recréer  une nouvelle activity

--- app/src/main/java/com/redpanda/pandarezzo/Cours.java
+++ app/src/main/java/com/redpanda/pandarezzo/Cours.java
@@ -1,8 +1,10 @@
 package com.redpanda.pandarezzo;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by samuel on 17/05/18.
@@ -14,13 +16,12 @@ public class Cours extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cours);
-        Button FlecheDroite = (Button) findViewById(R.id.flech_texte);
-        FlecheDroite.setOnClickListener(new View.OnClickListener(){
+        ImageButton flecheDroite = (ImageButton) findViewById(R.id.flech_texte);
+        flecheDroite.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Log.i("OC_RSS", "Cliquer ici");
+            public void onClick(View view) {
+                //TODO mettre action du bouton
             }
         });
-
     }
 }
