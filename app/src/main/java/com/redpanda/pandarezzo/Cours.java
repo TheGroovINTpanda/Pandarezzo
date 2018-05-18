f05a9e89a1b50116d69b7cf0f2ce39d4e8300721
Sam <samuel.rac@telecom-sudparis.eu>
2018-05-18 22:22:37 +0200

    Ajout des méthodes pour faire défiler les cours.

--- app/src/main/java/com/redpanda/pandarezzo/Cours.java
+++ app/src/main/java/com/redpanda/pandarezzo/Cours.java
@@ -1,10 +1,12 @@
 package com.redpanda.pandarezzo;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by samuel on 17/05/18.
@@ -12,19 +14,49 @@ import android.widget.ImageButton;
 
 public class Cours extends AppCompatActivity {
 
+    private int i = 0;
+    private int[] views = {R.drawable.first_lesson_02, R.drawable.first_lesson_03, R.drawable.first_lesson_04};
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cours);
-        int[] views = new int[]{};
-        //TODO remplir views
-        ImageButton flecheTexte = (ImageButton) findViewById(R.id.flech_texte);
+
+        ImageButton flecheTexte = findViewById(R.id.flech_texte);
         flecheTexte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.cours2);
-                ImageButton flecheGauche = (ImageButton) findViewById(R.id.fleche_gauche);
-                ImageButton flecheDroite = (ImageButton) findViewById(R.id.fleche_droite);
+                ImageButton flecheGauche = findViewById(R.id.fleche_gauche);
+                flecheGauche.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ImageView cours = findViewById(R.id.cours);
+                        if (0 <= i && i < views.length) {
+                            cours.setImageResource(views[i]);
+                            i--;
+
+                        } else {
+                            i = 0;
+                            cours.setImageResource(views[i]);
+                        }
+                    }
+                });
+
+                ImageButton flecheDroite = findViewById(R.id.fleche_droite);
+                flecheDroite.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ImageView cours = findViewById(R.id.cours);
+                        if (0 <= i && i < views.length) {
+                            cours.setImageResource(views[i]);
+                            i++;
+                        } else {
+                            i = 0;
+                            cours.setImageResource(views[i]);
+                        }
+                    }
+                });
             }
         });
     }
